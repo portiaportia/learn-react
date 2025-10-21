047637763c184cce9b75faaad152925614b16551
portiaportia <portiaplante@gmail.com>
2025-10-21 12:17:44 -0400

    awer

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import './css/index.css';
 import Home from './pages/Home';
 import About from './pages/About';
 
+export default function App() {
+  return (
+    <h1>I"m an app</h1>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <h1>Hi Everyone</h1>
-    <h2>Loving this</h2>
-    <Home />
-    <About />
+    <App />
   </React.StrictMode>
 );
