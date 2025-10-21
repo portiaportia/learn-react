a4624a7004ebe5d51825df62a0ec2bfcc94455dc
portiaportia <portiaplante@gmail.com>
2025-10-21 12:30:30 -0400

    waer

--- src/index.js
+++ src/index.js
@@ -2,12 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import './css/index.css';
+import Layout from './Layout';
 import Home from './pages/Home';
 import About from './pages/About';
 
 export default function App() {
   return (
-    <h1>I"m an app</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />}/>
+          <Route path="about" element={<About />}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 };
 
