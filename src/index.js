e5151bbd5ab6608f140dd3f157f62026a06e8277
portiaportia <portiaplante@gmail.com>
2025-10-23 11:26:17 -0400

    adding backround image

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
-import './css/index.css';
 import Layout from './Layout';
 import Home from './pages/Home';
 import About from './pages/About';
 import Houses from './pages/Houses';
 import Contact from './pages/Contact';
-import "./css/index.css";
 
 export default function App() {
   return (
