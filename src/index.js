cfa3228b35e365e737110bd4b4d5882f9cb8a2ad
portiaportia <portiaplante@gmail.com>
2025-10-23 09:21:18 -0400

    adjusted package.json

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import About from './pages/About';
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />}/>
