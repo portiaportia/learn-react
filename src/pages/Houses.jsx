61275d707325c5683acb41a195a3343ea1ac4746
portiaportia <portiaplante@gmail.com>
2025-11-04 12:12:12 -0500

    awer

--- src/pages/Houses.jsx
+++ src/pages/Houses.jsx
@@ -1,14 +1,27 @@
+import {useState, useEffect} from "react";
+import axios from "axios";
 import "./../css/House.css";
 import House from "../components/House";
 
 const Houses = () => {
+    const [houses, setHouses] = useState([]);
+
+    //after page has loaded
+    useEffect(()=>{
+        const loadHouses = async() => {
+            const response = await axios.get("https://portiaportia.github.io/json/house-plans.json");
+        };
+
+        loadHouses();
+    },[]);
+
     return (
         <main id="contact" className="main-content">
             <h2>Houses</h2>
             <div id="house-list" className="columns">
-                <House name="Farm House" num_bedrooms="5" />
-                <House name="Mansion" num_bedrooms="10" />
-                <House name="Family Home" num_bedrooms="3" />
+                {houses.map((house)=>(
+                    <House name={house.name} num_bedrooms={house.bedrooms} />
+                ))}
             </div>
             
         </main>
