d3ae4f91a0f9b2539ca121345cfc1a14e5840c4b
portiaportia <portiaplante@gmail.com>
2025-11-05 12:27:29 -0500

    fixed contact and made house list component

--- src/pages/Houses.jsx
+++ src/pages/Houses.jsx
@@ -1,36 +1,13 @@
 import {useState, useEffect} from "react";
 import axios from "axios";
 import "./../css/House.css";
-import House from "../components/House";
+import HouseList from "../components/HouseList";
 
 const Houses = () => {
-    const [houses, setHouses] = useState([]);
-
-    //after page has loaded
-    useEffect(()=>{
-        const loadHouses = async() => {
-            const response = await axios.get("https://portiaportia.github.io/json/house-plans.json");
-            setHouses(response.data);
-        };
-
-        loadHouses();
-    },[]);
-
     return (
-        <main id="contact" className="main-content">
+        <main id="houses" className="main-content">
             <h2>Houses</h2>
-            <div id="house-list" className="columns">
-                {houses.map((house)=>(
-                    <House  key={house._id} 
-                            id={house._id}
-                            name={house.name} 
-                            size={house.size}
-                            bedrooms={house.bedrooms}
-                            bathrooms={house.bathrooms}
-                            main_image={house.main_image} />
-                ))}
-            </div>
-            
+            <HouseList num="9" />
         </main>
     )
 };
